938f7698c061e1ca41105129e201cb5ee3a1eb86
Abdullah1Mansoor <bscs21043@itu.edu.pk>
2024-10-07 18:59:51 +0500

    add a profile component

---
+++ BSCS21043-ASM02(partA)/combiningTemplates/src/components/Profile.jsx
@@ -0,0 +1,15 @@
+// src/components/UserProfile.jsx
+
+import './Profile.css';
+
+function UserProfile() {
+  return (
+    <div className="user-profile">
+      <img src="https://via.placeholder.com/100" alt="Profile" />
+      <h3>User Name</h3>
+      <p>Email: user@example.com</p>
+    </div>
+  );
+}
+
+export default UserProfile;
